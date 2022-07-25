5e2099867062f429b8230882647dffe02f1294d0
Ivor Gradinski <ivorgradinski@gmail.com>
2022-07-25 12:59:39 +0200

    friends

--- app.routes.js
+++ app.routes.js
@@ -31,6 +31,7 @@ function RoutesConfig($stateProvider, $urlRouterProvider) {
     .state("friends", {
       url: "/friends",
       templateUrl: "./views/friends/friends.html",
+      controller: "friendsController",
     });
 
   $urlRouterProvider.otherwise("/");

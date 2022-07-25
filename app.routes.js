0ff8d16d23159d8dfc4989f14ab00ca07d71ec39
ivanbakotin <ivan.bakotin1999@gmail.com>
2022-07-25 16:00:46 +0200

    routing solved badly

--- app.routes.js
+++ app.routes.js
@@ -1,14 +1,9 @@
-angular.module("app.routes", ["ui.router"]).config(RoutesConfig);
+angular.module("app.routes", ["ui.router"]).config(RoutesConfig).run(AuthGuard);
 
 RoutesConfig.$inject = ["$stateProvider", "$urlRouterProvider"];
 
 function RoutesConfig($stateProvider, $urlRouterProvider) {
   $stateProvider
-    .state("home", {
-      url: "/",
-      templateUrl: "./views/profile/profile.html",
-      controller: "ProfileController",
-    })
     .state("login", {
       url: "/login",
       templateUrl: "./views/login/login.html",
@@ -19,20 +14,76 @@ function RoutesConfig($stateProvider, $urlRouterProvider) {
       templateUrl: "./views/register/register.html",
       controller: "RegisterController",
     })
+    .state("home", {
+      url: "/",
+      templateUrl: "./views/profile/profile.html",
+      controller: "ProfileController",
+      resolve: {
+        auththenticate: [
+          "$q",
+          function ($q) {
+            if (AuthGuard()) {
+              return $q.resolve("AUTH_SUCCESS");
+            } else {
+              return $q.reject("AUTH_REQUIRED");
+            }
+          },
+        ],
+      },
+    })
     .state("profile", {
       url: "/profile",
       templateUrl: "./views/profile/profile.html",
       controller: "ProfileController",
+      resolve: {
+        auththenticate: [
+          "$q",
+          function ($q) {
+            if (AuthGuard()) {
+              return $q.resolve("AUTH_SUCCESS");
+            } else {
+              return $q.reject("AUTH_REQUIRED");
+            }
+          },
+        ],
+      },
     })
     .state("games", {
       url: "/games",
       templateUrl: "./views/games/games.html",
+      resolve: {
+        auththenticate: [
+          "$q",
+          function ($q) {
+            if (AuthGuard()) {
+              return $q.resolve("AUTH_SUCCESS");
+            } else {
+              return $q.reject("AUTH_REQUIRED");
+            }
+          },
+        ],
+      },
     })
     .state("friends", {
       url: "/friends",
       templateUrl: "./views/friends/friends.html",
-      controller: "FriendsController",
+      resolve: {
+        auththenticate: [
+          "$q",
+          function ($q) {
+            if (AuthGuard()) {
+              return $q.resolve("AUTH_SUCCESS");
+            } else {
+              return $q.reject("AUTH_REQUIRED");
+            }
+          },
+        ],
+      },
     });
 
   $urlRouterProvider.otherwise("/");
+}
+
+function AuthGuard() {
+  return localStorage.getItem("token");
 }

184cd3964fffa991533080930be89fc898ca5cec
akrapovicivan <iakrap00@fesb.hr>
2022-07-26 14:20:48 +0200

    update photo works

--- views/profile/editProfile.drct.js
+++ views/profile/editProfile.drct.js
@@ -1,8 +1,24 @@
-angular.module("gamerApp").directive("editProfile", editProfile);
+angular
+  .module("gamerApp")
+  .directive("editProfile", editProfile)
+  .directive("customOnChange", customOnChange);
 
 function editProfile() {
   var ddo = {
     templateUrl: "./views/profile/editprofile.html",
   };
   return ddo;
+}
+
+function customOnChange() {
+  return {
+    restrict: "A",
+    link: function (scope, element, attrs) {
+      var onChangeHandler = scope.$eval(attrs.customOnChange);
+      element.on("change", onChangeHandler);
+      element.on("$destroy", function () {
+        element.off();
+      });
+    },
+  };
 }

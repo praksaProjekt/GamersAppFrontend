1c4df00c9b3f6b2c2f5870543e887b9fa8b5da64
ivanbakotin <ivan.bakotin1999@gmail.com>
2022-07-27 14:32:42 +0200

    video

--- views/profile/profile.ctrl.js
+++ views/profile/profile.ctrl.js
@@ -24,30 +24,59 @@ angular.module("gamerApp").controller("ProfileController", [
       epicGames: "",
       steam: "",
       file: "",
+      video: "",
+    };
+
+    $scope.$onInit = () => {
+      var video = document.getElementById("video");
+      video.pause();
     };
 
     profile.switchEdit = function () {
       profile.edit = !profile.edit;
     };
+
     profile.saveChanges = function () {
       console.log(profile.model);
       profile.switchEdit();
     };
+
     $scope.updateImage = function () {
       var input = document.getElementById("input").files[0];
       var image = document.getElementById("img");
+
       var reader = new FileReader();
       reader.addEventListener(
         "load",
         function () {
           image.src = this.result;
           profile.model.file = this.result;
-          console.log(profile.model.file);
           image.classList.remove("editImg");
         },
         false
       );
+
       reader.readAsDataURL(input);
+    };
+
+    $scope.updateVideo = function () {
+      var videoInput = document.getElementById("videoInput").files[0];
+
+      var source = document.getElementById("source");
+
+      var reader = new FileReader();
+      reader.addEventListener(
+        "load",
+        function () {
+          profile.model.video = this.result;
+          source.src = this.result;
+          video.load();
+          video.play();
+        },
+        false
+      );
+
+      reader.readAsDataURL(videoInput);
     };
   },
 ]);

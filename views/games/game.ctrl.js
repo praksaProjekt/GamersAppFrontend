742f56d16da9260fc208e6e5227f93754effb6ba
Ivor Gradinski <ivorgradinski@gmail.com>
2022-07-29 13:16:48 +0200

    addGames

---
+++ views/games/game.ctrl.js
@@ -0,0 +1,20 @@
+angular.module("gamerApp").controller("GamesController", [
+  "$scope",
+  "editProfile",
+
+  function ($scope, editProfile) {
+    var games = this;
+
+    games.gamesData = {};
+
+    games.model = {
+      userId: 0,
+      gameId: 0,
+      gamerTag: "",
+    };
+
+    games.addGame = function () {
+      games.model.userId = editProfile.getClaimObjectId();
+    };
+  },
+]);
